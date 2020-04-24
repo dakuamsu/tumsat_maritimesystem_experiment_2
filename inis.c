31a36992d5906f645bbfce8a57029e7d80dbdf45
dakumasu <darkmaster0529@gmail.com>
2020-04-24 19:07:58 +0900

    SFMT

--- inis.c
+++ inis.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <SFMT.h>
 
 
 
@@ -11,10 +12,11 @@ int sim(){
 
 int main(void){
   srand(time(NULL));
-  double x, y;
+  double x, y, z, w;
   double a = 54;
   double b = 60 / 4 + 48;
   x = rand() / (double) RAND_MAX;
   y = 1 / a * log(x);
-  printf("%f\n",y);
+  z = rand() / (double) RAND_MAX;
+
 }
