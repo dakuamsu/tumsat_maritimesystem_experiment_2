983fa596849826bdc1f84eda6c3c1def5a3c7361
dakumasu <darkmaster0529@gmail.com>
2020-04-24 16:09:35 +0900

    標準偏差を出す

--- inis.c
+++ inis.c
@@ -3,6 +3,20 @@
 #include <time.h>
 #include <math.h>
 
+double sig(double b){
+  double k;
+  int i = 0;
+  double ave = 0;
+  int sum = 0;
+  srand(time(NULL));
+  while (ave == b){
+  k = rand() / (double) RAND_MAX;
+  i ++;
+  sum += k;
+  ave = sum / i;
+  }
+}
+
 int sim(){
 
 }
@@ -10,8 +24,9 @@ int sim(){
 int main(void){
   srand(time(NULL));
   double x, y;
-  int a = 58;
+  double a = 54;
+  double b = 60 / 4 + 48;
   x = rand() / (double) RAND_MAX;
-  y = 1 / a * exp(-1 * x / a);
-  print(y)
+  y = 1 / a * log(x);
+  printf("%f\n",y);
 }
