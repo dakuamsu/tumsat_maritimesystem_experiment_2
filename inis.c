e37239f0ffef8397d4eab6e948fb6b7717d5deff
Dakumasu <darkmaster0529@gmail.com>
2020-05-29 00:26:03 +0900

    Update inis.c
    fix algorithm

--- inis.c
+++ inis.c
@@ -3,40 +3,34 @@
 #include <time.h>
 #define _USER_MATH_DEFINES
 #include <math.h>
-#include "SFMT.h"
 
-/* 入ってくるコンテナの関数 */
 double in(double a){
-  sfmt_t sfmt
   double x, y;
   srand(time(NULL));
-  x = sfmt_genrand_real2(&sfmt) ;
+  x = rand() / (double) RAND_MAX;
   y = -1 / a * log(x);
   return y;
 }
 
-/* 裁くコンテナの関数 */
 double out(double pi, double sig, double b){
-  sfmt_t sfmt
   double s, t, o;
-  s = sfmt_genrand_real2(&sfmt) ;
-  t = sfmt_genrand_real2(&sfmt) ;
-  o = sqrt(-2 * log(s)) * cos(2 * pi * t)* sig;
+  srand(time(NULL));
+  s = rand() / (double) RAND_MAX;
+  t = rand() / (double) RAND_MAX;
+  o = fabs(sqrt(-2 * log(s)) * cos(2 * pi * t)* sig);
   return o;
 }
 
 int main(void){
-  sfmt_t sfmt;
-  int seed = time(NULL);
-  sfmt_init_gen_rand(&sfmt, seed);
+  srand(time(NULL));
   double x, y, s, t;
-  double a = 54;  /* 18 * 3 */
-  double b = 60 / 4 + 48;  /* 学籍番号下二桁のやつ */
-  double sig = 0.01; /* 標準偏差わからんから適当に入れる */
-  double pi = 3.14159265358979323846;  /* 円周率の定義 */
-  double time = 0;  /* 時間の初期化 */
-  int yard = 0;  /* ヤードにあるコンテナの数の初期化 */
-  int done = 0;  /* 捌いた数の初期化 */
+  double a = 54;
+  double b = 60 / 4 + 48;
+  double sig = 0.1;
+  double pi = 3.14159265358979323846;
+  double time = 0;
+  int yard = 0;
+  int done = 0;
   s = t = 0;
   x = in(a);
   yard += 1;
@@ -44,11 +38,11 @@ int main(void){
   t += s;
   y = out(pi,sig,b);
   t += y;
-  int i = 1;
+  int i = 0;
   while(i != 100){
+      i += 1;
       printf("%d個目入場 ヤードには%d個 %f時間経過\n", i, yard, s);
       x = in(a);
-      i += 1;
       s += x;
       if(s > t){
           yard -= 1;
@@ -57,7 +51,8 @@ int main(void){
           printf("%f\n",y);
           printf("%d個目捌いた ヤードには%d個 %f時間経過\n", done, yard, t);
           if(yard == 0){
-              t += s;
+              t = s;
+              t += y;
           }
           else{
               t += y;
